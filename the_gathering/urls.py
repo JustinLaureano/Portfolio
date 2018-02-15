5d90f47a21f6e9da8cbdacaa0645700253731148
Justin Laureano <gravatron87@gmail.com>
2018-02-14 19:09:33 -0500

    added html links

--- the_gathering/urls.py
+++ the_gathering/urls.py
@@ -7,4 +7,12 @@ app_name = 'the_gathering'
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('eat/', views.eat_page, name='eat'),
+    path('drink/', views.drink_page, name='drink'),
+    path('shop/', views.shop_page, name='shop'),
+    path('listen/', views.listen_page, name='listen'),
+    path('about/', views.about_page, name='about'),
+    path('menus/', views.menus_page, name='menus'),
+    path('private_events/', views.private_events_page, name='private_events'),
+    path('reservations/', views.reservations_page, name='reservations'),
+    path('contact/', views.contact_page, name='contact'),
 ]
--- the_gathering/views.py
+++ the_gathering/views.py
@@ -7,3 +7,35 @@ def home_page(request):
 
 def eat_page(request):
     return render(request, 'the_gathering/eat.html', {})
+
+
+def drink_page(request):
+    return render(request, 'the_gathering/drink.html', {})
+
+
+def shop_page(request):
+    return render(request, 'the_gathering/shop.html', {})
+
+
+def listen_page(request):
+    return render(request, 'the_gathering/listen.html', {})
+
+
+def about_page(request):
+    return render(request, 'the_gathering/about.html', {})
+
+
+def menus_page(request):
+    return render(request, 'the_gathering/about.html', {})
+
+
+def private_events_page(request):
+    return render(request, 'the_gathering/private_events.html', {})
+
+
+def reservations_page(request):
+    return render(request, 'the_gathering/reservations.html', {})
+
+
+def contact_page(request):
+    return render(request, 'the_gathering/contact.html', {})
