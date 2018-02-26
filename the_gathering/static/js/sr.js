485080900a07a85600957435e293786d77926b66
Justin Laureano <gravatron87@gmail.com>
2018-02-26 18:19:51 -0500

    updated scrollReveal

---
+++ the_gathering/static/js/sr.js
@@ -0,0 +1,292 @@
+window.sr = ScrollReveal();
+
+// index
+sr.reveal('header', {
+duration: 2000,
+origin: 'bottom'
+});
+sr.reveal('#welcome-left', {
+duration: 2000,
+origin: 'left',
+distance: '800px'
+});
+sr.reveal('#welcome-right', {
+duration: 2000,
+origin: 'right',
+distance: '800px'
+});
+sr.reveal('#private-stamp', {
+duration: 2000,
+origin: 'left',
+distance: '800px'
+});
+sr.reveal('#private-text', {
+duration: 2000,
+origin: 'right',
+distance: '800px'
+});
+sr.reveal('#artwork', {
+duration: 2000,
+origin: 'bottom',
+});
+sr.reveal('.hor-rule', {
+duration: 2000,
+origin: 'top',
+});
+sr.reveal('.hours-of-op', {
+duration: 2000,
+origin: 'bottom',
+});
+sr.reveal('.address', {
+duration: 2000,
+origin: 'bottom',
+distance: '500px'
+});
+sr.reveal('#carousel-container', {
+duration: 2000,
+origin: 'right',
+distance: '800px'
+});
+sr.reveal('#hero-image', {
+duration: 2000,
+delay: 800,
+origin: 'top',
+distance: '200px'
+});
+sr.reveal('#event-list', {
+duration: 2000,
+delay: 1000,
+origin: 'bottom',
+distance: '200px'
+});
+sr.reveal('#footer', {
+duration: 2000,
+delay: 200,
+origin: 'bottom',
+distance: '200px'
+});
+
+// eat
+sr.reveal('#eat-intro', {
+duration: 2000,
+origin: 'bottom',
+});
+sr.reveal('#eat-dinner-left', {
+duration: 2000,
+origin: 'left',
+distance: '800px'
+});
+sr.reveal('#eat-dinner-right', {
+duration: 2000,
+origin: 'right',
+distance: '800px'
+});
+sr.reveal('#eat-lunch-left', {
+duration: 2000,
+delay: 300,
+origin: 'left',
+distance: '800px'
+});
+sr.reveal('#eat-lunch-right', {
+duration: 2000,
+delay: 300,
+origin: 'right',
+distance: '800px'
+});
+sr.reveal('#eat-private-left', {
+duration: 2000,
+origin: 'left',
+distance: '800px'
+});
+sr.reveal('#eat-private-right', {
+duration: 2000,
+origin: 'right',
+distance: '800px'
+});
+
+// drink
+sr.reveal('#drink-intro', {
+duration: 2000,
+origin: 'bottom',
+});
+sr.reveal('#drink-wine-left', {
+duration: 2000,
+origin: 'left',
+distance: '800px'
+});
+sr.reveal('#drink-wine-right', {
+duration: 2000,
+origin: 'right',
+distance: '800px'
+});
+sr.reveal('#drink-whiskey-left', {
+duration: 2000,
+delay: 300,
+origin: 'left',
+distance: '800px'
+});
+sr.reveal('#drink-whiskey-right', {
+duration: 2000,
+delay: 300,
+origin: 'right',
+distance: '800px'
+});
+sr.reveal('#drink-cocktail-left', {
+duration: 2000,
+origin: 'left',
+distance: '800px'
+});
+sr.reveal('#drink-cocktail-right', {
+duration: 2000,
+origin: 'right',
+distance: '800px'
+});
+
+// shop
+sr.reveal('#shop-intro', {
+duration: 2000,
+origin: 'bottom',
+});
+sr.reveal('#shop-gift-basket-left', {
+duration: 2000,
+origin: 'left',
+distance: '800px'
+});
+sr.reveal('#drink-gift-basket-right', {
+duration: 2000,
+origin: 'right',
+distance: '800px'
+});
+sr.reveal('#shop-wine-left', {
+duration: 2000,
+delay: 300,
+origin: 'left',
+distance: '800px'
+});
+sr.reveal('#shop-wine-right', {
+duration: 2000,
+delay: 300,
+origin: 'right',
+distance: '800px'
+});
+sr.reveal('#shop-giftcard-left', {
+duration: 2000,
+origin: 'left',
+distance: '800px'
+});
+sr.reveal('#shop-giftcard-right', {
+duration: 2000,
+origin: 'right',
+distance: '800px'
+});
+// listen
+sr.reveal('#listen-intro', {
+duration: 2000,
+origin: 'bottom',
+});
+sr.reveal('#listen-event-image', {
+duration: 2000,
+origin: 'bottom'
+});
+sr.reveal('#listen-event-text', {
+duration: 2000,
+delay: 200,
+origin: 'top',
+distance: '150px'
+});
+
+// about
+sr.reveal('#about-intro', {
+duration: 2000,
+origin: 'bottom',
+});
+sr.reveal('#about-gathering-hero', {
+duration: 2000,
+origin: 'bottom',
+distance: '200px'
+});
+sr.reveal('#history', {
+duration: 2000,
+origin: 'bottom',
+distance: '200px'
+});
+sr.reveal('#about-history-left', {
+duration: 2000,
+delay: 300,
+origin: 'left',
+distance: '500px'
+});
+sr.reveal('#about_text', {
+duration: 2000,
+delay: 300,
+origin: 'right',
+distance: '500px'
+});
+sr.reveal('#about_rest', {
+duration: 2000,
+origin: 'bottom',
+distance: '500px'
+});
+sr.reveal('#about-gathering-text', {
+duration: 2000,
+delay: 300,
+origin: 'left',
+distance: '500px'
+});
+sr.reveal('#about-wine-box', {
+duration: 2000,
+origin: 'bottom',
+});
+sr.reveal('#about-wine-title', {
+duration: 2000,
+delay: 500,
+origin: 'top',
+});
+sr.reveal('#about-wine-text', {
+duration: 2000,
+delay: 500,
+origin: 'bottom',
+});
+sr.reveal('#about-wine-hero', {
+duration: 2000,
+origin: 'bottom',
+distance: '0px'
+});
+sr.reveal('#about-patio-box', {
+duration: 2000,
+origin: 'bottom',
+});
+sr.reveal('#about-patio-title', {
+duration: 2000,
+delay: 500,
+origin: 'top',
+});
+sr.reveal('#about-patio-text', {
+duration: 2000,
+delay: 500,
+origin: 'bottom',
+});
+sr.reveal('#about_anvil', {
+duration: 2000,
+origin: 'bottom',
+distance: '500px'
+});
+sr.reveal('#about-anvil-text', {
+duration: 2000,
+delay: 300,
+origin: 'right',
+distance: '500px'
+});
+// menus
+sr.reveal('#myTab', {
+duration: 2000,
+origin: 'bottom',
+});
+sr.reveal('#menu-all', {
+duration: 2000,
+delay: 400,
+origin: 'bottom',
+});
+// private events
+// reservations
+// contact
