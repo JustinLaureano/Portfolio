74d9e5b6a338714215eff88a384ff31f64be17da
Justin Laureano <gravatron87@gmail.com>
2018-03-17 17:35:53 -0400

    finished responsive footer

--- the_gathering/static/js/main.js
+++ the_gathering/static/js/main.js
@@ -49,4 +49,70 @@ $(document).ready(function () {
         }
     });
 
+    $(window).resize(function() {
+        let inline =    `
+        <div class="col-12 footer-nav">
+            <ul class="list-inline" >
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:eat' %}">Eat</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:drink' %}">Drink</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:shop' %}">Shop</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:events' %}">Events</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:about' %}">About</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:menus' %}">Menus</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:private_events' %}">Private</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:reservations' %}">Reservations</a></li>
+            </ul>        
+        </div>
+        `;
+
+        let oneColumn = `
+        <div class="col-12 footer-nav">
+        <ul class="list-unstyled" >
+            <li class="px-3"><a href="{% url 'the_gathering:eat' %}">Eat</a></li>
+            <li class="px-3"><a href="{% url 'the_gathering:drink' %}">Drink</a></li>
+            <li class="px-3"><a href="{% url 'the_gathering:shop' %}">Shop</a></li>
+            <li class="px-3"><a href="{% url 'the_gathering:events' %}">Events</a></li>
+            <li class="px-3"><a href="{% url 'the_gathering:about' %}">About</a></li>
+            <li class="px-3"><a href="{% url 'the_gathering:menus' %}">Menus</a></li>
+            <li class="px-3"><a href="{% url 'the_gathering:private_events' %}">Private</a></li>
+            <li class="px-3"><a href="{% url 'the_gathering:reservations' %}">Reservations</a></li>
+        </ul>        
+    </div>
+        `;
+
+        let twoColumn = `
+        <div class="col-12 footer-nav">
+            <ul class="list-inline" >
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:eat' %}">Eat</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:drink' %}">Drink</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:shop' %}">Shop</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:events' %}">Events</a></li>
+            </ul>        
+        </div>
+        <div class="col-12 footer-nav">
+            <ul class="list-inline" >
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:about' %}">About</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:menus' %}">Menus</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:private_events' %}">Private</a></li>
+                <li class="list-inline-item px-3"><a href="{% url 'the_gathering:reservations' %}">Reservations</a></li>
+            </ul>        
+        </div>
+        `;
+
+        if ($(window).width() < 580) {
+           console.log('less');
+           $('#footer-nav-list').text('');
+           $('#footer-nav-list').append(oneColumn);
+
+        } else if ($(window).width() < 880) {
+            console.log('more');
+            $('#footer-nav-list').text('');
+            $('#footer-nav-list').append(twoColumn);
+       } else {
+            $('#footer-nav-list').text('');
+            $('#footer-nav-list').append(inline);
+       }
+       
+    });
+
 });
