297f62b01bcb460da6189edb17124c019f87a692
Justin Laureano <gravatron87@gmail.com>
2018-03-22 18:50:28 -0400

    modified portfolio website

--- justin_laureano/urls.py
+++ justin_laureano/urls.py
@@ -6,7 +6,5 @@ app_name = 'justin_laureano'
 # /justin_laureano/
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('about/', views.about_page, name='about'),
-    path('project/', views.project_page, name='project'),
-    path('contact/', views.contact_page, name='contact'),
+    path('project_1/', views.the_gathering, name='project_1'),
 ]
--- justin_laureano/views.py
+++ justin_laureano/views.py
@@ -1,18 +1,10 @@
 from django.shortcuts import render
-from .models import Project
+# from .models import Project
 
 
 def home_page(request):
     return render(request, 'justin_laureano/index.html', {})
 
 
-def about_page(request):
-    return render(request, 'justin_laureano/about.html', {})
-
-
-def project_page(request):
-    return render(request, 'justin_laureano/project.html', {})
-
-
-def contact_page(request):
-    return render(request, 'justin_laureano/contact.html', {})
+def the_gathering(request):
+    return render(request, 'justin_laureano/the_gathering.html', {})
