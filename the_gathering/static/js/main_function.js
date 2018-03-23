f60e9c2657e0c9e2b94307a029a421e3d762ce3c
Justin Laureano <gravatron87@gmail.com>
2018-03-23 10:38:08 -0400

    added responsive navbar/header

--- the_gathering/static/js/main_function.js
+++ the_gathering/static/js/main_function.js
@@ -1,34 +1,16 @@
-$(document).ready(function () {
-    var windowHeight = $(window).height();
-    var bodyHeight = $('.container-fluid').height();
-    var footerHeight = $('footer').height();
-    console.log(windowHeight);
-    console.log(bodyHeight);
-    console.log(footerHeight);
-
-    if (windowHeight > bodyHeight) {
-        $('footer').addClass('short');
-    };
-
-    $(window).resize(function() {
-        var windowHeight = $(window).height();
-        var docHeight = bodyHeight;
-
-        console.log("windowheight" + windowHeight);
-        console.log("bodyheight" + docHeight);
-
-        if (windowHeight > docHeight) {
-            $('footer').addClass('short');
-        } else {
-            $('footer').removeClass('short');
-        }
-
-        if(windowHeight < 1024) {
-            $('#index').addClass('m-t-10');
-        } else {
-            $('#index').removeClass('m-t-10');
-        }
-    });
-
-
+$(window).scroll(function() {
+  var windowHeight = $(this).height();
+  var windowScrollTop = $(this).scrollTop();
+  console.log(windowHeight);
+  console.log(windowScrollTop);
+  
+  if (windowHeight <= windowScrollTop) {
+      $('.header').css({'position' : 'fixed',
+                    'top' : '0px',
+                    'left' : '0px',
+                    'transform' : 'translate(0px, 0px)'});
+    //   $('.logo-sm').css({'transform': 'translate(0px, 75px)'});
+      console.log('now');
+      $('.projects').css({'margin-top' : '80px'});
+  };
 });
